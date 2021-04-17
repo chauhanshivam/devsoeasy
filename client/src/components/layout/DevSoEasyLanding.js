1a363ece50839c912a8784b3a08557600c8df4db
Shivam Chauhan <42336027+chauhanshivam@users.noreply.github.com>
2021-04-18 02:31:47 +0530

    Initial commit

---
+++ client/src/components/layout/DevSoEasyLanding.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const DevSoEasyLanding = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
+
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Developer Sooo Easy!!!!!</h1>
+          <p className='lead'>
+            A one stop solution for software developer to find job , take help on new tech and much more....
+            </p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn btn-light'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+DevSoEasyLanding.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps)(DevSoEasyLanding);
